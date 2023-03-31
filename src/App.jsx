6f09676f274fdf92282e3876737dcc145c837e75
muraneth <muran.eth@gmail.com>
2023-03-31 20:46:49 +0800

    index form

--- src/App.jsx
+++ src/App.jsx
@@ -394,6 +394,80 @@ function App() {
           </Form>
         </div>
 
+
+ {/* ---------------------- */}
+
+
+        <Form
+      form={form}
+      name="dynamic_form_complex"
+      onFinish={onFinish}
+      style={{ maxWidth: 600 }}
+      autoComplete="off"
+    >
+      <Form.Item name="area" label="Area" rules={[{ required: true, message: 'Missing area' }]}>
+        <Select options={areas} onChange={handleChange} />
+      </Form.Item>
+      <Form.List name="sights">
+        {(fields, { add, remove }) => (
+          <>
+            {fields.map((field) => (
+              <Space key={field.key} align="baseline">
+                <Form.Item
+                  noStyle
+                  shouldUpdate={(prevValues, curValues) =>
+                    prevValues.area !== curValues.area || prevValues.sights !== curValues.sights
+                  }
+                >
+                  {() => (
+                    <Form.Item
+                      {...field}
+                      label="Sight"
+                      name={[field.name, 'sight']}
+                      rules={[{ required: true, message: 'Missing sight' }]}
+                    >
+                      <Select disabled={!form.getFieldValue('area')} style={{ width: 130 }}>
+                        {(sights[form.getFieldValue('area') as SightsKeys] || []).map((item) => (
+                          <Option key={item} value={item}>
+                            {item}
+                          </Option>
+                        ))}
+                      </Select>
+                    </Form.Item>
+                  )}
+                </Form.Item>
+                <Form.Item
+                  {...field}
+                  label="Price"
+                  name={[field.name, 'price']}
+                  rules={[{ required: true, message: 'Missing price' }]}
+                >
+                  <Input />
+                </Form.Item>
+
+                <MinusCircleOutlined onClick={() => remove(field.name)} />
+              </Space>
+            ))}
+
+            <Form.Item>
+              <Button type="dashed" onClick={() => add()} block icon={<PlusOutlined />}>
+                Add sights
+              </Button>
+            </Form.Item>
+          </>
+        )}
+      </Form.List>
+      <Form.Item>
+        <Button type="primary" htmlType="submit">
+          Submit
+        </Button>
+      </Form.Item>
+    </Form>
+    {/* ---------------------- */}
+
+
+
+
         <div>
           <h2> Step4: Preview a collection</h2>
           <Space align="start">
